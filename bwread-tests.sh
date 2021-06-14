1b0164404314aac101a748a4ae5986c22bf1b27d
Michael Bechtel <mbechtel@ku.edu>
2021-06-14 12:22:54 -0500

    ToC Revision Changes
    Replace LatencyRead(LLC) with BwRead(DRAM) victim

--- bwread-tests.sh
+++ bwread-tests.sh
@@ -2,22 +2,40 @@
 
 . ./functions
 
-declare -a corunners=("BwRead" "LatencyRead" "BankRead" "BwWrite" "LatencyWrite" "BankWrite")
-declare -a functions=(BwRead LatencyRead BankRead BwWrite LatencyWrite BankWrite)
+declare -a corunners=("BwRead" "PLLRead" "BkPLLRead" "BwWrite" "PLLWrite" "BkPLLWrite")
+declare -a functions=(BwRead PLLRead BkPLLRead BwWrite PLLWrite BkPLLWrite)
 declare -a benchmark=(bandwidth latency-mlp attacker3 bandwidth latency-mlp attacker3)
 
-> bwread-results.txt
-index=0
+# BwRead (LLC) tests
+> bwread-llc-results.txt
+count=0
 for corun in "${functions[@]}"
 do
-	echo "${corunners[$count]}" >> bwread-results.txt
-	BwReadVictim 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-results.txt
+	echo "${corunners[$count]}" >> bwread-llc-results.txt
+	BwReadVictim 48 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-llc-results.txt
 	for i in 1 2 3
 	do
 		$corun $i &> /dev/null &
-		BwReadVictim 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-results.txt
+		BwReadVictim 48 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-llc-results.txt
 	done
 	killall "${benchmark[$count]}"
 	count=$((count+1))
-	echo "" >> bwread-results.txt
+	echo "" >> bwread-llc-results.txt
+done
+
+# BwRead (DRAM) tests
+> bwread-dram-results.txt
+count=0
+for corun in "${functions[@]}"
+do
+	echo "${corunners[$count]}" >> bwread-dram-results.txt
+	BwReadVictim 16384 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-dram-results.txt
+	for i in 1 2 3
+	do
+		$corun $i &> /dev/null &
+		BwReadVictim 16384 2> /dev/null | grep average | awk 'NF { print $(NF-1) }' >> bwread-dram-results.txt
+	done
+	killall "${benchmark[$count]}"
+	count=$((count+1))
+	echo "" >> bwread-dram-results.txt
 done
--- gen-dat.py
+++ gen-dat.py
@@ -1,35 +1,35 @@
 import numpy as np
 
-bwread_results = open("bwread-results.txt", "r")
+bwread_llc_results = open("bwread-llc-results.txt", "r")
 dat_vals = []
 for i in range(6):
     dat_vals.append([])
-    dat_vals[-1].append(bwread_results.readline()[:-1])
+    dat_vals[-1].append(bwread_llc_results.readline()[:-1])
     for j in range(4):
         if j == 0:
-            solo = float(bwread_results.readline())
+            solo = float(bwread_llc_results.readline())
             dat_vals[-1].append(1)
         else:
-            temp = float(bwread_results.readline())
+            temp = float(bwread_llc_results.readline())
             temp = temp / solo
             dat_vals[-1].append(temp)
-    bwread_results.readline()
-bwread_results.close()
+    bwread_llc_results.readline()
+bwread_llc_results.close()
 np.savetxt("figures/Fig6.dat", dat_vals, delimiter="\t", fmt="%s")
 
-latencyread_results = open("latencyread-results.txt", "r")
+bwread_dram_results = open("bwread-dram-results.txt", "r")
 dat_vals = []
 for i in range(6):
     dat_vals.append([])
-    dat_vals[-1].append(latencyread_results.readline()[:-1])
+    dat_vals[-1].append(bwread_dram_results.readline()[:-1])
     for j in range(4):
         if j == 0:
-            solo = float(latencyread_results.readline())
+            solo = float(bwread_dram_results.readline())
             dat_vals[-1].append(1)
         else:
-            temp = float(latencyread_results.readline())
+            temp = float(bwread_dram_results.readline())
             temp = np.around(temp / solo, decimals=2)
             dat_vals[-1].append(temp)
-    latencyread_results.readline()
-latencyread_results.close()
+    bwread_dram_results.readline()
+bwread_dram_results.close()
 np.savetxt("figures/Fig7.dat", dat_vals, delimiter="\t", fmt="%s")
